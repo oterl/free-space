194516db8c1fc7a95021587519894414508fa3cd
Oleksandr <terletskyi.oleksandr@gmail.com>
2019-03-05 22:17:06 +0200

    Minor changes

--- src/utils/random-color.ts
+++ src/utils/random-color.ts
@@ -1,2 +1,2 @@
 // tslint:disable-next-line
-export const randomColor = () => '0x' + Math.floor(Math.random() * 16777215).toString(16)
+export const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16)
