351acbc7096417c564232cb474812f8e2bc98a49
Oleksandr <terletskyi.oleksandr@gmail.com>
2019-03-07 20:47:56 +0200

    Changed initial configuration a little

--- src/app/initial-config.ts
+++ src/app/initial-config.ts
@@ -1,8 +1,8 @@
 const initialSpace = 20
 const initialMaxTry = 100
 const initialStep = 1
-const initialEps = 1
-const initialK = 3
+const initialEps = 3
+const initialK = 114
 const initialSphereSize = 2
 const initialMaxSphereCount = 100
 
